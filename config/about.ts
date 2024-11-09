0d34197d967ee59fb2bb551e2d4c44a32aaf5c5a
zhwcreate <3331598351@qq.com>
2024-11-09 16:37:45 +0800

    feat: 修改about描述

--- config/about.ts
+++ config/about.ts
@@ -1,10 +1,6 @@
 export const heroTitle = {
   template: [
-    {
-      type: 'h1',
-      text: `Hi , I'm `,
-      class: ' font-light text-4xl font-900 inline-block',
-    },
+    { type: 'h1', text: `Hi, I'm `, class: ' font-light text-4xl font-900 inline-block' },
     { type: 'h1', text: `zw`, class: ' text-4xl font-bold inline-block' },
     {
       type: 'h1',
@@ -12,22 +8,17 @@ export const heroTitle = {
       class:
         ' font-light text-4xl font-bold inline-block hover:scale-[1.05] cursor-pointer origin-center transition-all',
     },
-    { type: 'h1', text: ` `, class: ' h-0 w-0 scale-0' },
+    { type: 'h1', text: ` `, class: ' h-0 w-0 leading-none' },
     {
       type: 'span',
-      text: 'A ',
-      class: 'font-light text-4xl font-900 inline-block mt-[5px]',
-    },
-    {
-      type: 'span',
-      text: ' JavaScript ',
+      text: 'A Javascript',
       class: 'font-light text-4xl font-900 inline-block mt-[5px]',
     },
     {
       type: 'code',
-      text: '<Learner />',
+      text: ' <Developer />',
       class:
-        ' inline-block font-medium mx-2 text-3xl rounded p-2 bg-gray-200 dark:bg-gray-800/0 hover:dark:bg-gray-800/100 bg-opacity-0 hover:bg-opacity-100 transition-background duration-200',
+        ' inline-block font-medium mx-2 text-3xl rounded p-2 bg-gray-200 dark:bg-gray-800/0 hover:dark:bg-gray-800/100 bg-opacity-0 hover:bg-opacity-100 transition-background duration-200"><div><span class="inline-block whitespace-pre',
     },
   ],
 };
