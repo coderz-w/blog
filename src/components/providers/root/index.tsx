4490f5ae0b0bf3fe91030b9b8b241f6bbd0a83fc
zhwcreate <3331598351@qq.com>
2024-11-11 16:19:14 +0800

    feat: analytics

--- src/components/providers/root/index.tsx
+++ src/components/providers/root/index.tsx
@@ -3,6 +3,7 @@
 import { PropsWithChildren } from 'react';
 import { ThemeProvider } from 'next-themes';
 import { LazyMotion } from 'framer-motion';
+import { Analytics } from '@vercel/analytics/react';
 
 import ProviderComposer from '@/components/modules/shared/ProviderComposer';
 
@@ -11,6 +12,7 @@ const loadFeatures = () => import('./framer-lazy-feature').then((res) => res.def
 const webAppContexts: JSX.Element[] = [
   <ThemeProvider children key="themeProvider" />,
   <LazyMotion features={loadFeatures} strict key="framer" />,
+  <Analytics />,
 ];
 
 export default function WebAppProviders({ children }: PropsWithChildren) {
